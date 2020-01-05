c2f6f5a694ef61a416be8624f9e5a920f8f23c2f
George <gwong323@yahoo.com>
2020-01-05 21:29:54 +0000

    dice roll random with timer

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
        
     @IBOutlet weak var rightDice: UIImageView!
     
+  
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +29,29 @@ class ViewController: UIViewController {
 
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        leftDice.image = #imageLiteral(resourceName: "DiceThree")
-          rightDice.image = #imageLiteral(resourceName: "DiceTwo")
- 
+    let diceArray  =  [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    var count = 0
         
-    }
+        var _ = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true){ t in
+            count += 1
+            let leftDiceIndex = Int.random(in: 0...5)
+            let rightDiceIndex = Int.random(in: 0...5)
+            self.leftDice.image = diceArray[leftDiceIndex]
+            self.rightDice.image = diceArray[rightDiceIndex]
+            print("left dice= \(leftDiceIndex + 1)")
+            if count >= 5 {
+                t.invalidate()
+            }
+        }
+            }
     
-}
+            
 
+    
+    
+
+
+    
+
+
+}
